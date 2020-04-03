25368f957982d7b1b36053b416f7ca6a5e8bff0f
Ganesh Annan <gannan@digitalbazaar.com>
2020-04-03 16:55:30 -0400

    Only expose `instances` as a part of the public API.

--- index.js
+++ index.js
@@ -3,41 +3,6 @@
  */
 'use strict';
 
-//import axios from 'axios';
-
-export {
-  getCapabilityAgent, findDocuments, getEdvDocument, getEdvClient
-} from './utils.js';
-
-export {default as Collection} from './Collection.js';
-
 import * as instances from './instances.js';
-import * as users from './users.js';
-// FIXME: disable configurations
-//import * as configurations from './configurations.js';
-export {instances, users};//, configurations};
 
-/*
-export async function registerIssuer({presentation, account}) {
-  // create the issuer instance and the issuer configuration
-  const {verifiableCredential: [credential]} = presentation;
-  const {instance} = await instances.create(
-    {controller: account.id, presentation});
-  await configurations.create({credential, account, instance});
-}
-
-export async function unregisterIssuer({issuer, account}) {
-  // first remove configuration
-  await configurations.remove({issuer, account});
-  // remove instance
-  return instances.remove({issuer});
-}
-
-export async function issue(
-  {issuer, flow, credentials, presentation}) {
-  const url = '/vc-issuer/issue';
-  const response = await axios.post(
-    url, {issuer, flow, credentials, presentation});
-  return response.data;
-}
-*/
+export {instances};
